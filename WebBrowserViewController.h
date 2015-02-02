a76964376d1a1c48e32cf641770eacdb75c2610c
Amanda Pi <amandapi88@gmail.com>
2015-02-01 16:33:10 -0800

    Functional web browser

---
+++ WebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Amanda Pi on 2015-01-30.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebBrowserViewController : UIViewController
+
+@end
---
+++ WebBrowserViewController.m
@@ -0,0 +1,207 @@
+//
+//  WebBrowserViewController.m
+//  BlocBrowser
+//
+//  Created by Amanda Pi on 2015-01-30.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "WebBrowserViewController.h"
+
+@interface WebBrowserViewController () <UIWebViewDelegate, UITextFieldDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UIButton *backButton;
+@property (nonatomic, strong) UIButton *forwardButton;
+@property (nonatomic, strong) UIButton *stopButton;
+@property (nonatomic, strong) UIButton *reloadButton;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, assign) NSUInteger frameCount;
+
+@end
+
+@implementation WebBrowserViewController
+
+#pragma mark - UIViewController
+
+- (void)loadView
+{
+    // override loadView to create a main container to place all subviews
+    UIView *mainView = [UIView new];
+    
+    self.webView = [[UIWebView alloc] init];
+    self.webView.delegate = self;
+    
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    // Assignment #4 - change placeholder text
+    // self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Search Google or enter an URL address", @"Placeholder text for web browser URL field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.Delegate = self;
+    
+    self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.backButton setEnabled:NO];
+    
+    self.forwardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.forwardButton setEnabled:NO];
+    
+    self.stopButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.stopButton setEnabled:NO];
+    
+    self.reloadButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.reloadButton setEnabled:NO];
+    
+    [self.backButton setTitle:NSLocalizedString(@"Back", @"Back command") forState:UIControlStateNormal];
+    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward command") forState:UIControlStateNormal];
+    [self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop command") forState:UIControlStateNormal];
+    [self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.reloadButton setTitle:NSLocalizedString(@"Refresh", @"Refresh command") forState:UIControlStateNormal];
+    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    
+    //[mainView addSubview:self.webView];
+    //[mainView addSubview:self.textField];
+    //[mainView addSubview:self.backButton];
+    //[mainView addSubview:self.forwardButton];
+    //[mainView addSubview:self.stopButton];
+    //[mainView addSubview:self.reloadButton];
+
+    // The following for-loop will replace the last 6 lines
+    for (UIView *viewToLoad in @[self.webView, self.textField, self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        [mainView addSubview:viewToLoad];
+    }
+    
+    self.view = mainView;
+}
+
+
+- (void)viewDidLoad
+    // Do any additional setup after loading the view.
+{
+    [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+}
+
+
+
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    // self.webView.frame = self.view.frame; We don't want the webView to fill the main page
+  
+    // First, calculate some dimentsions
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight - itemHeight;
+    CGFloat buttonWidth = CGRectGetWidth(self.view.bounds)/4;
+    
+    // Now assign the frames
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+    
+    CGFloat currentButtonX = 0;
+    
+    for (UIButton *thisButton in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        thisButton.frame = CGRectMake(currentButtonX, CGRectGetMaxY(self.webView.frame), buttonWidth, itemHeight); currentButtonX += buttonWidth;
+    }
+    
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    
+    NSString *URLString = textField.text;
+  
+    NSURL *URL = [NSURL URLWithString:URLString];
+    
+    //Assignment #1 to #3
+    NSRange spaceRange = [URLString rangeOfString:@" "];
+    NSString *stringADD = [URLString stringByReplacingCharactersInRange:spaceRange withString:@"+"];
+    
+    // if space exists
+    if (spaceRange.location) {
+    NSString *searchURL = [NSString stringWithFormat:@"http://google.com/search?q=%@", stringADD];
+    URL = [NSURL URLWithString:searchURL];
+    }
+    
+    // if space does not exist
+    if (!URL.scheme && !spaceRange.location) {
+    URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+    // load the corresponding URL
+    if (URL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        [self.webView loadRequest:request];
+    }
+    
+    return NO;
+    }
+
+
+#pragma mark - UIWebViewDelegate
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    self.frameCount++;
+    [self updateButtonsAndTitle];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    self.frameCount--;
+    [self updateButtonsAndTitle];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    if (error.code !=-999) {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error") message:[error localizedDescription] delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+    [alert show];
+    }
+    [self updateButtonsAndTitle];
+    self.frameCount--;
+}
+
+#pragma mark - Miscellaneous
+
+- (void) updateButtonsAndTitle
+{
+    NSString *webpageTitle = [self.webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+                              
+    if (webpageTitle) {
+        self.title = webpageTitle;
+    } else {
+        self.title = self.webView.request.URL.absoluteString;
+    }
+    
+    if (self.frameCount > 0) {
+        [self.activityIndicator startAnimating];
+    } else {
+        [self.activityIndicator stopAnimating];
+    }
+                              
+    self.backButton.enabled = [self.webView canGoBack];
+    self.forwardButton.enabled = [self.webView canGoForward];
+    self.stopButton.enabled = self.frameCount > 0;
+    self.reloadButton.enabled = !self.frameCount == 0;
+    
+}
+
+@end
